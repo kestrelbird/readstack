8a404d971b802de67fa8c335fee2a3b6f6682302
Monika Pustuła <moonika.pustua@gmail.com>
2022-10-27 22:45:09 +0200

    add source db - context.xml, add DataSourceProvider class

---
+++ src/main/java/com/monikapustula/readstack/db/DataSourceProvider.java
@@ -0,0 +1,22 @@
+package com.monikapustula.readstack.db;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DataSourceProvider {
+    private static DataSource dataSource;
+
+    private DataSourceProvider() {
+    }
+
+    public static DataSource getDataSource() throws NamingException {
+        if (dataSource == null) {
+            Context context = new InitialContext();
+            Context envContext = (Context) context.lookup("java:comp/env/");
+            dataSource = (DataSource) envContext.lookup("jdbc/readstack");
+        }
+        return dataSource;
+    }
+}
