db4121e9dd6543a9018dec7739f3f8e0ec0dec56
Monika Pustuła <moonika.pustua@gmail.com>
2022-11-08 23:00:09 +0100

    add password encryption

--- src/main/java/com/monikapustula/readstack/domain/api/UserService.java
+++ src/main/java/com/monikapustula/readstack/domain/api/UserService.java
@@ -2,7 +2,9 @@ package com.monikapustula.readstack.domain.api;
 
 import com.monikapustula.readstack.domain.user.User;
 import com.monikapustula.readstack.domain.user.UserDao;
+import org.apache.commons.codec.digest.DigestUtils;
 
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
 
 public class UserService {
@@ -11,7 +13,17 @@ public class UserService {
 
     public void register(UserRegistration userRegistration) {
         User userToRegister = userRegistrationMapper(userRegistration);
-        userDao.save(userToRegister);
+        try {
+            hashPasswordWithSha256(userToRegister);
+            userDao.save(userToRegister);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void hashPasswordWithSha256(User user) throws NoSuchAlgorithmException {
+        String sha256Password = DigestUtils.sha256Hex(user.getPassword());
+        user.setPassword(sha256Password);
     }
 
     private static User userRegistrationMapper(UserRegistration userRegistration) {
